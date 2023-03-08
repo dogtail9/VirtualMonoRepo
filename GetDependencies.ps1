d759caf0f61fab30699c165d27ca2fdc84fc1036
Christer Eriksson <christer@dogtail.se>
2023-03-08 14:41:56 +0100

    Add d3js

---
+++ GetDependencies.ps1
@@ -0,0 +1,54 @@
+$allProjectFiles = Get-ChildItem -Path . -Filter *.csproj -exclude *.UnitTests.csproj, *.Specs.csproj -Recurse | Resolve-Path -Relative
+
+$projects = @()
+
+foreach ($projfile in $allProjectFiles)
+{
+    
+    $projectReferences = Select-Xml -Path $projfile -XPath '/Project/ItemGroup/ProjectReference' | ForEach-Object { $_.Node.Include }
+    # write-host $projfile
+    # write-host $t
+    # write-host 
+
+    $projectReferencesArray = @()
+    foreach($projRef in $projectReferences)
+    {
+        $t = $projRef -split "\\"#[IO.Path]::PathSeparator
+        $r = $t[$t.Length-1].replace(".csproj","")
+        $projectReferencesArray += $r
+    }
+
+    Write-Host $r
+    $project = @{
+        Project = (Get-Item $projfile).Basename
+        References = $projectReferencesArray
+    }
+
+    $projects += $project
+    
+}
+
+$nodes = @()
+$links = @()
+foreach($p in $projects)
+{
+    $nodes += @{id = $p.Project}
+
+    Write-Host "proj: " + $p.Project
+    foreach($r in $p.References)
+    {
+        $links += @{
+            from=$p.Project
+            to=$r
+        }
+        Write-Host "   ref: " + $r
+    }
+    Write-Host
+}
+
+$root = @{
+    nodes = $nodes
+    links = $links
+}
+$json = $root | ConvertTo-Json
+Write-Host $json;
